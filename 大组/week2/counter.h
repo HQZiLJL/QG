15774de2319f3ff6c4cf2788c2983ebed10d62d6
HQZhen12 <157691025+HQZiLJL@users.noreply.github.com>
2024-03-29 00:00:21 +0800

    biggroup counter

---
+++ 大组/week2/counter.h
@@ -0,0 +1,50 @@
+/***************************************************************************************
+ *	File Name				:	linklist.h
+ *	CopyRight				:	2024 QG Studio
+ *	SYSTEM					:   win11
+ *	Create Data				:	2024.3.21
+ *
+ *
+ *--------------------------------Revision History--------------------------------------
+ *	No	version		Data			Revised By			Item			Description
+ *
+ *
+ ***************************************************************************************/
+ /**************************************************************
+  *	Multi-Include-Prevent Section
+  **************************************************************/
+
+#ifndef LINKLIST_H_INCLUDED
+#define LINKLIST_H_INCLUDED
+
+  /**************************************************************
+   *	Macro Define Section
+   **************************************************************/
+
+#define MAXSIZE 200
+int q;
+
+/**************************************************************
+ *	Struct Define Section
+ **************************************************************/
+
+ // 定义结点
+typedef struct
+{
+	int data[MAXSIZE];
+	int top;
+}STACK;
+
+STACK* stack_create();
+void stack_create2(STACK** ptr);
+int stack_isempty(STACK* ptr);
+int stack_isfull(STACK* ptr);
+int stack_push(STACK* ptr, const int* x);
+int stack_pop(STACK* ptr, int* x);
+int stack_top(STACK* ptr, int* x);  //取栈顶元素查看,不出栈
+void stack_display(STACK* ptr);
+void stack_destroy(STACK* ptr);
+/**************************************************************
+ *	End-Multi-Include-Prevent Section
+ **************************************************************/
+#endif
